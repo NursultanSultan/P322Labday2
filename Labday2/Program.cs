54d1fe2f0cbad86ab7eb5a573a8c750b02324afc
NursultanSultan <tu201906251@code.edu.az>
2022-03-04 09:46:02 -0800

    I added tasks

---
+++ Labday2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Labday2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region ededlerin sayi
+            //int number = Convert.ToInt32(Console.ReadLine());
+
+            //int count = 0;
+
+            //while (number >= 1)
+            //{
+            //    number = number / 10;
+            //    count = count + 1;
+            //}
+
+            //Console.WriteLine($"{count} reqemlidir");
+            #endregion
+
+            #region array task about min max value
+            //int[] numbers = { 12, 56, 12, 34, 13 };
+            //int max = numbers[0];
+            //int min = numbers[0];
+            //int indexmin = 0;
+
+            //for (int i = 1; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] > max)
+            //    {
+            //        max = numbers[i];
+            //    }
+            //    if (numbers[i] <= min)
+            //    {
+            //        min = numbers[i];
+            //        indexmin = i;
+            //    }
+            //}
+
+            //Console.WriteLine($"max eded : {max} \nmin eded : {min} ");
+            #endregion
+        }
+    }
+}
